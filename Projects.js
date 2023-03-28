fc4bd5a877635a00ba1a61f013cf1cee52c8a97a
phbui <101759075+phbui@users.noreply.github.com>
2023-03-27 20:51:13 -0400

    added website showcase

--- Projects.js
+++ Projects.js
@@ -3,41 +3,37 @@ function initSlider(slider) {
   const prevBtn = slider.querySelector('.arrow.left');
   const nextBtn = slider.querySelector('.arrow.right');
   const dots = slider.querySelectorAll('.dot');
-
   let currentImageIndex = 0;
-  images[currentImageIndex].classList.add('active');
-  dots[currentImageIndex].classList.add('active');
 
-  prevBtn.addEventListener('click', () => {
+  const setActiveImageAndDot = (index) => {
     images[currentImageIndex].classList.remove('active', 'zoomed');
     dots[currentImageIndex].classList.remove('active');
-    currentImageIndex = currentImageIndex - 1;
-    if (currentImageIndex < 0) {
-      currentImageIndex = images.length - 1;
-    }
+    currentImageIndex = index;
     images[currentImageIndex].classList.add('active');
     dots[currentImageIndex].classList.add('active');
+
+    console.log(images[currentImageIndex].dataset.text)
+    updateProjectInfo(
+      slider,
+      images[currentImageIndex].dataset.link,
+      images[currentImageIndex].dataset.text,
+      images[currentImageIndex].dataset.date,
+      images[currentImageIndex].dataset.collab
+    )
+  };
+
+  prevBtn.addEventListener('click', () => {
+    const index = currentImageIndex > 0 ? currentImageIndex - 1 : images.length - 1;
+    setActiveImageAndDot(index);
   });
 
   nextBtn.addEventListener('click', () => {
-    images[currentImageIndex].classList.remove('active', 'zoomed');
-    dots[currentImageIndex].classList.remove('active');
-    currentImageIndex = currentImageIndex + 1;
-    if (currentImageIndex >= images.length) {
-      currentImageIndex = 0;
-    }
-    images[currentImageIndex].classList.add('active');
-    dots[currentImageIndex].classList.add('active');
+    const index = currentImageIndex < images.length - 1 ? currentImageIndex + 1 : 0;
+    setActiveImageAndDot(index);
   });
 
   dots.forEach((dot, dotIndex) => {
-    dot.addEventListener('click', () => {
-      images[currentImageIndex].classList.remove('active', 'zoomed');
-      dots[currentImageIndex].classList.remove('active');
-      currentImageIndex = dotIndex;
-      images[currentImageIndex].classList.add('active');
-      dots[currentImageIndex].classList.add('active');
-    });
+    dot.addEventListener('click', () => setActiveImageAndDot(dotIndex));
   });
 
   images.forEach((image) => {
@@ -56,4 +52,23 @@ function initSlider(slider) {
       }
     });
   });
+
+  setActiveImageAndDot(currentImageIndex);
 }
+
+function updateProjectInfo(slider, link, linkText, date, collab) {
+
+  const proj = slider.parentElement;
+
+  const projectLink = proj.querySelector('#link');
+  const projectLinkText = proj.querySelector('#link-text');
+  const projectDate = proj.querySelector('#date');
+  const projectCollab = proj.querySelector('#collab');
+
+
+  if (link) projectLink.href = link;
+  if (linkText) projectLinkText.textContent = "🔗 " + linkText;
+  if (date) projectDate.textContent = date;
+  if (collab) projectCollab.textContent = collab;
+}
+
