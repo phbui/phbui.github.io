6a434e9bb951bb573abf3c050161319eb016a339
Φ Bui <bilphui@gmail.com>
2023-07-02 13:14:11 -0700

    added more stops

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,31 +1,68 @@
-import React, { useEffect, useRef, useCallback, useState } from 'react';
+/* The code is a React component called `ModelViewer` that renders a 3D model viewer using the Three.js
+library. It imports necessary dependencies such as React, Three.js, STLLoader, lodash, and TWEEN. */
+import React, { useEffect, useRef, useCallback, useState, useMemo } from 'react';
 import * as THREE from 'three';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-import { debounce } from 'lodash';
+import { debounce, throttle } from 'lodash';
 import TWEEN from '@tweenjs/tween.js';
+
+/**
+ * The function generates interpolated camera settings based on a given array of settings and the
+ * number of steps.
+ * @returns The function `generateCameraSettings` returns an array of camera settings.
+ */
+const generateCameraSettings = (settings, steps) => {
+  const newSettings = [];
+
+  for (let i = 0; i < settings.length - 1; i++) {
+    for (let j = 0; j <= steps; j++) {
+      const t = j / steps;
+      const interpPosition = {
+        x: settings[i].position.x * (1 - t) + settings[i + 1].position.x * t,
+        y: settings[i].position.y * (1 - t) + settings[i + 1].position.y * t,
+        z: settings[i].position.z * (1 - t) + settings[i + 1].position.z * t
+      };
+      const interpRotation = {
+        x: settings[i].rotation.x * (1 - t) + settings[i + 1].rotation.x * t,
+        y: settings[i].rotation.y * (1 - t) + settings[i + 1].rotation.y * t,
+        z: settings[i].rotation.z * (1 - t) + settings[i + 1].rotation.z * t
+      };
+      newSettings.push({ position: interpPosition, rotation: interpRotation });
+    }
+  }
+
+  // Add the last setting
+  newSettings.push(settings[settings.length - 1]);
+
+  return newSettings;
+};
 
 const ModelViewer = React.memo(() => {
   const containerRef = useRef(null);
   const sceneRef = useRef(null);
   const cameraRef = useRef(null);
   const rendererRef = useRef(null);
-  const defaultX = 0;
-  const defaultY = 0.7;
-  const defaultZ = 1.25;
 
-  const cameraPositionsAndRotations = [
+  const [defaultX, defaultY, defaultZ] = useMemo(() => [0, 0.7, 1.25], []);
+  
+  const cameraPositionsAndRotations = useMemo(() => [
     { position: { x: defaultX, y: defaultY, z: defaultZ }, rotation: { x: 0, y: 0, z: 0 } },
     { position: { x: 1, y: 1, z: 0 }, rotation: { x: 0, y: 2, z: 0 } },
     { position: { x: -1, y: 1, z: 0 }, rotation: { x: 0, y: -2, z: 0 } },
     { position: { x: 0, y: 1, z: -2 }, rotation: { x: 0, y: -3, z: 0 } },
-    // Add more as needed
-  ];
+  ], [defaultX, defaultY, defaultZ]);
+
+  const numInterpolationSteps = 10; // Change this to modify the number of steps
+  const interpolatedCameraPositionsAndRotations = useMemo(() => generateCameraSettings(cameraPositionsAndRotations, numInterpolationSteps), [cameraPositionsAndRotations, numInterpolationSteps]);
 
   const [currentCameraSettingIndex, setCameraSettingIndex] = useState(0);
 
   const fpsLimit = 30; // limit to 30 frames per second
   let then = 0;
   
+/* The `animate` function is a callback function that is used to update and render the scene at a
+specific frame rate. It uses the `requestAnimationFrame` method to continuously call itself,
+creating a loop for rendering the scene. */
   const animate = useCallback((now = 0) => {
     const elapsed = now - then;
     const fpsInterval = 1000 / fpsLimit;
@@ -38,26 +75,32 @@ const ModelViewer = React.memo(() => {
     requestAnimationFrame(animate);
   }, []);
 
+/* The `handleResize` function is a debounced callback function that is used to handle the window
+resize event. It is created using the `useCallback` hook and the `debounce` function from the lodash
+library. */
   const handleResize = useCallback(debounce(() => {
     cameraRef.current.aspect = window.innerWidth / window.innerHeight;
     cameraRef.current.updateProjectionMatrix();
     rendererRef.current.setSize(window.innerWidth, window.innerHeight);
   }, 200), []);
-  
-  const handleScroll = useCallback((event) => {
+
+/* The `handleScroll` function is a throttled callback function that is used to handle the scroll
+event. It is created using the `useCallback` hook and the `throttle` function from the lodash
+library. */
+  const handleScroll = useCallback(throttle((event) => {
     const delta = Math.sign(event.deltaY);
     let nextIndex = currentCameraSettingIndex + delta;
   
     // Ensure index is within bounds of our array
     nextIndex = Math.max(0, nextIndex);
-    nextIndex = Math.min(cameraPositionsAndRotations.length - 1, nextIndex);
+    nextIndex = Math.min(interpolatedCameraPositionsAndRotations.length - 1, nextIndex);
   
     if (nextIndex !== currentCameraSettingIndex) {
       const currentPosition = { x: cameraRef.current.position.x, y: cameraRef.current.position.y, z: cameraRef.current.position.z };
       const currentRotation = { x: cameraRef.current.rotation.x, y: cameraRef.current.rotation.y, z: cameraRef.current.rotation.z };
   
       new TWEEN.Tween(currentPosition)
-        .to(cameraPositionsAndRotations[nextIndex].position, 2000)
+        .to(interpolatedCameraPositionsAndRotations[nextIndex].position, 2000)
         .easing(TWEEN.Easing.Quadratic.Out)
         .onUpdate(() => {
           cameraRef.current.position.set(currentPosition.x, currentPosition.y, currentPosition.z);
@@ -65,7 +108,7 @@ const ModelViewer = React.memo(() => {
         .start();
   
       new TWEEN.Tween(currentRotation)
-        .to(cameraPositionsAndRotations[nextIndex].rotation, 2000)
+        .to(interpolatedCameraPositionsAndRotations[nextIndex].rotation, 2000)
         .easing(TWEEN.Easing.Quadratic.Out)
         .onUpdate(() => {
           cameraRef.current.rotation.set(currentRotation.x, currentRotation.y, currentRotation.z);
@@ -74,12 +117,10 @@ const ModelViewer = React.memo(() => {
   
       setCameraSettingIndex(nextIndex);
     }
-    console.log(`Current camera position: x=${cameraRef.current.position.x}, y=${cameraRef.current.position.y}, z=${cameraRef.current.position.z}`);
-    console.log(`Current camera rotation: x=${cameraRef.current.rotation.x}, y=${cameraRef.current.rotation.y}, z=${cameraRef.current.rotation.z}`);
-  
-  }, [currentCameraSettingIndex]);
-  
+  }, 200), [currentCameraSettingIndex, interpolatedCameraPositionsAndRotations]);
 
+/* The `useEffect` hook is used to perform side effects in a React component. In this case, the effect
+is responsible for setting up the initial state of the 3D model viewer. */
   useEffect(() => {
     let scene, camera, renderer;
 
@@ -99,7 +140,10 @@ const ModelViewer = React.memo(() => {
       containerRef.current.appendChild(renderer.domElement);
     }
 
-    // Load the model
+
+    /* The code is using the `STLLoader` from the Three.js library to load a 3D model file in STL format.
+    It creates a new instance of the `STLLoader` and then calls its `load` method, passing in the file
+    path of the STL model and a callback function. */
     const loader = new STLLoader();
     loader.load('src/assets/gundam.stl', (geometry) => {
       const material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true, wireframeLinewidth: 0.5});
@@ -120,6 +164,16 @@ const ModelViewer = React.memo(() => {
     // Animation loop
     animate();
 
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeChild(renderer.domElement);
+      }
+    };
+  }, [animate, defaultX, defaultY, defaultZ]);
+
+/* The `useEffect` hook is used to add event listeners for the `resize` and `wheel` events when the
+component is mounted. The event listeners are added using the `window.addEventListener` method. */
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
     window.addEventListener('wheel', handleScroll);
 
@@ -127,11 +181,8 @@ const ModelViewer = React.memo(() => {
     return () => {
       window.removeEventListener('resize', handleResize);
       window.removeEventListener('wheel', handleScroll);
-      if (containerRef.current) {
-        containerRef.current.removeChild(renderer.domElement);
-      }
     };
-  }, [animate, handleResize, handleScroll, currentCameraSettingIndex]);
+  }, [handleResize, handleScroll]);
 
   return <div ref={containerRef} />;
 });
