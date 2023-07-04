776306281a5f0f36c1ab2c27c2e5fdb4064c23ab
Φ Bui <bilphui@gmail.com>
2023-07-03 20:25:04 -0700

    refactored styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,43 @@
 import React from 'react';
 import ModelViewer from './components/ModelViewer';
-
-const styles = {
-  mainContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  background: {
-    width: '100%',
-    height: '100vh',
-    position: 'fixed',
-    top: 0,
-    backgroundImage: "url('src/assets/background.jpg')",
-    backgroundSize: 'contain',
-    backgroundPosition: 'center',
-    zIndex: 0,
-  },
-  canvasHolder: {
-    position: 'fixed',
-    top: 0,
-    zIndex: 1,
-  },
-  accentHolder:{
-    width: '100%',
-    height: '100%',
-    position: 'fixed',
-    top: 0,
-    zIndex: 3,
-    pointerEvents: 'none',
-  },
-  topLeft: {
-    position: 'absolute',
-    top: '25px',
-    left: '25px',
-  },
-  bottomRight: {
-    position: 'absolute',
-    bottom: '25px',
-    right: '25px',
-  },
-  accentText: {
-    margin: 0,
-    fontFamily: "'Courier New', Courier, monospace",
-    fontSize: '30px',
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  verticalText: {
-    writingMode: 'vertical-rl'
-  },
-  textBox: {
-    margin: '300px 0 300px 0',
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    border: '1px solid red',
-    color: 'white',
-    boxShadow: '0 0 5px rgb(254, 33, 70,0.25), 0 0 15px rgb(254, 33, 70, 0.5)',
-    padding: '25px',
-    height: ' 400vh',
-    zIndex: 2,
-  }
-}
+import './App.css';
 
 const App = () => {
   return (
-    <div style={styles.mainContainer}>
-      <div style={styles.background}></div>
-      <div style={styles.canvasHolder}>
+    <div className='mainContainer'>
+      <div className='background'></div>
+      <div className='canvasHolder'>
         <ModelViewer />
       </div>
-      <div style={styles.accentHolder}>      
-        <div style={styles.topLeft}>
-          <p style={{...styles.accentText, ...styles.verticalText}}>+---|=============-</p>
+      <div className='accentHolder'>      
+        <div className='topLeft'>
+          <p className='accentText verticalText'>+---|=============-</p>
         </div>
-        <div style={styles.topLeft}>        
-          <p style={styles.accentText}>+---|=============-</p>
+        <div className='topLeft'>        
+          <p className='accentText'>+---|=============-</p>
         </div>
-        <div style={{...styles.bottomRight, right: '62px'}}>
-          <p style={{...styles.accentText, ...styles.verticalText}}>-=============|---+</p>
+        <div className='bottomRight' style={{right: '62px'}}>
+          <p className='accentText verticalText'>-=============|---+</p>
         </div>
-        <div style={{...styles.bottomRight}}>
-        <p style={styles.accentText}>-=============|---+</p>
+        <div className='bottomRight'>
+        <p className='accentText'>-=============|---+</p>
         </div>
       </div>
-      <div style={styles.textBox}>
+      <div className='textBox'>
         <p>halsjdklasjlkjljfljs</p>
+      </div>
+      <div className="textBox">
+      <pre>{"const About = (phi_bui) => {"}</pre>
+      <pre>     {"let undergrad = 'B.S. in CS @ WPI';"}</pre>
+      <pre>     {"let grad = null; "}<span>{"//TBD"}</span></pre>
+      <pre>{"}"}</pre>
+      </div>
+      <div className='textBox'>
+        <pre>{"const Contact = {"}</pre>
+        <pre>    {"{email: "}<a className='links' href="mailto:bilphui@gmail.com">bilphui@gmail.com</a>{"},"}</pre>
+        <pre>    {"{linkedin: "}<a className='links' href="https://www.linkedin.com/in/phi-bui/">phi-bui</a>{"},"}</pre>
+        <pre>    {"{github: "}<a className='links' href="https://github.com/phbui">phbui</a>{"}"}</pre>
+        <pre>{"}"}</pre>
       </div>
     </div>
   );
