6eec620cf70d1198956a48cfcf83944a7eb4dc92
Φ Bui <bilphui@gmail.com>
2023-07-07 14:15:37 -0700

    made images clickable + increased onhover clarity

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ import ALink from './ALink'
 const Project = ({img, title, link, text, download}) => {
   return (
     <div className='projectItem'>
-        <img className='projectIMG' src={img}/>
+        <a href={link} download={download}><img className='projectIMG' src={img}/></a>
         <div className='projectTextHolder'>
           <pre className='preText projectText' ><a href={link} download={download}><ALink text={title}/></a></pre>
           <pre className='preText projectText'>{text}</pre>
